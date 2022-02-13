f31d3557ff77ce73a370f4641ff96e6b602bdef5
Miguel Escobar <mae2136@gmail.com>
2022-02-13 15:48:09 -0600

    JS: - Pseudo code for work required to be done.

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,19 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Create generatePassword() function that generates the following:
+
+var userCriteria;
+// Prompt for 4 password criteria: 
+// length (8 - 128 characters)
+// character types: lower case
+// character type: upper case
+// character types: numeric
+// character types: special characters
+
+// Note all selections in an array that gets sizes based on selection
+
+// Validate input, minimum of one character type is needed (ensure array is not empty)
+
+// Display generated password in alert or written on page
