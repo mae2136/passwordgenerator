bdb4d674ae0d9f6f087bf1e8f641c26b6a14c806
Miguel Escobar <mae2136@gmail.com>
2022-02-14 21:37:06 -0600

    script.js: - Created selectCategory function: Allows user to select pw category types. Validates category types to ensure at least one is selected.
    - Created selectLength function:
    Asks user to input length of the password.
    Validates to ensure only a number is selected.

--- Develop/script.js
+++ Develop/script.js
@@ -14,17 +14,66 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // Create generatePassword() function that generates the following:
-
-var userCriteria;
 // Prompt for 4 password criteria: 
-// length (8 - 128 characters)
 // character types: lower case
 // character type: upper case
 // character types: numeric
 // character types: special characters
-
 // Note all selections in an array that gets sizes based on selection
+function selectCategory() {
+  var choice = [];
+  choice.push(confirm("Would would like your passsword to contain Lower Case characters (e.g., g, n)?"));
+  choice.push(confirm("Would would like your passsword to contain Upper Case characters (e.g., G, N)?"));
+  choice.push(confirm("Would would like your passsword to contain Numeric characters (e.g., 7, 5)?"));
+  choice.push(confirm("Would would like your passsword to contain Special characters (e.g., &, $)?"));
+  console.log(choice);
+  // Validate input, minimum of one character type is needed (ensure array is not empty)
+  for (let index = 0; index < choice.length; index++) {
+    if (choice[index]) {
+      break;
+    } 
+    else if (index == 3) {
+      alert("Error: You must select at least one pasword criteria. Please try again");
+      choice = selectCategory();
+    }
+  }
+  console.log(choice);
+  
+  return choice;
+}
 
-// Validate input, minimum of one character type is needed (ensure array is not empty)
+// length (8 - 128 characters)
+function selectLength () {
+  var pwlength = prompt("How long would you like your password to be?\n Please enter a Number between 8 and 128");
+  
+  // Validate pw length for non number values
+  pwlength = Number(pwlength);
+  console.log(pwlength);
+  
+  // Validates pwlength to ensure only a number has been input.
+  while (isNaN(pwlength)) {
+    alert("Error: Password length must be a Number. Please try again");
+    pwlength = prompt("How long would you like your password to be?\n Please enter a number between 8 and 128");
+  }
+  // Function found at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN
+
+  // Validate pwlength for valid number values
+  while (pwlength < 8 || pwlength > 128) {
+    alert("Error: Password length must be a Number between 8 and 128 characters. Please try again");
+    pwlength = prompt("How long would you like your password to be?\n Please enter a number between 8 and 128");
+  }
+  console.log(pwlength);
+  return pwlength;
+}
+
+function generatePassword() {
+  // Have user select password categories and length.
+  var userChoice = selectCategory();
+  console.log(userChoice);
+  
+  var userLength = selectLength();
+  console.log(userLength);
+  // Generate a password based on user criteria
+}
 
 // Display generated password in alert or written on page
