0c7b4ed30118adfbe26b4bed3e572291d778e66c
Miguel Escobar <mae2136@gmail.com>
2022-02-16 21:37:45 -0600

    script.js: - Create two new functions for password validation and random passwords.
    - Make generatePassword() run continously until a valid password is found.
    
    - Reformat of code to account for new functions.

--- Develop/script.js
+++ Develop/script.js
@@ -30,31 +30,30 @@ function selectCategory() {
   for (let index = 0; index < choice.length; index++) {
     if (choice[index]) {
       break;
-    } 
+    }
     else if (index == 3) {
       alert("Error: You must select at least one pasword criteria. Please try again");
       choice = selectCategory();
     }
   }
-  console.log(choice);
-  
+
   return choice;
 }
 
 // length (8 - 128 characters)
-function selectLength () {
+function selectLength() {
   var pwlength = prompt("How long would you like your password to be?\n Please enter a Number between 8 and 128");
-  
+
   // Validate pw length for non number values
   pwlength = Number(pwlength);
-  
+
   // Validates pwlength to ensure only a number has been input.
   while (isNaN(pwlength)) {
     alert("Error: Password length must be a Number. Please try again");
     pwlength = prompt("How long would you like your password to be?\n Please enter a number between 8 and 128");
   }
   // Function found at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN
-
+  
   // Validate pwlength for valid number values
   while (pwlength < 8 || pwlength > 128) {
     alert("Error: Password length must be a Number between 8 and 128 characters. Please try again");
@@ -63,11 +62,84 @@ function selectLength () {
   return pwlength;
 }
 
+// Randomize password
+function randomPassword(allowable, userLength) {
+  var password = [];
+  // Display generated password in alert or written on page
+  for (let index = 0; index < userLength; index++) {
+    password = password + allowable[Math.floor(Math.random() * allowable.length)];
+  }
+  console.log(password);
+  return password;
+}
+
+// Ensure password matches user selection.
+function validation(password, userChoice) {
+  var validation = [];
+  var compChoice = [false, false, false, false];
+  for (let index = 0; index < password.length; index++) {
+    validation[index] = password.charCodeAt(index); // https://www.w3schools.com/js/js_string_methods.asp
+  }
+
+  // Validate Lower Case
+  if (userChoice[0]) {
+    for (let index = 0; index < validation.length; index++) {
+      if ((97 <= validation[index]) && (validation[index] <= 122)) {
+        compChoice[0] = true;
+        break;
+      }
+    }
+  }
+  // Validate Upper Case
+  if (userChoice[1]) {
+    for (let index = 0; index < validation.length; index++) {
+      if ((65 <= validation[index]) && (validation[index] <= 90)) {
+        compChoice[1] = true;
+        break;
+      }
+    }
+  }
+  // Validate Number
+  if (userChoice[2]) {
+    for (let index = 0; index < validation.length; index++) {
+      if ((48 <= validation[index]) && (validation[index] <= 57)) {
+        compChoice[2] = true;
+        break;
+      }
+    }
+  }
+
+  // Validate Special
+  if (userChoice[3]) {
+    for (let index = 0; index < validation.length; index++) {
+      if ((32 <= validation[index]) && (validation[index] <= 47)) {
+        compChoice[3] = true;
+        break;
+      }
+      else if ((58 <= validation[index]) && (validation[index] <= 64)) {
+        compChoice[3] = true;
+        break;
+      }
+      else if ((91 <= validation[index]) && (validation[index] <= 96)) {
+        compChoice[3] = true;
+        break;
+      }
+      else if ((123 <= validation[index]) && (validation[index] <= 126)) {
+        compChoice[3] = true;
+        break;
+      }
+    }
+  }
+
+  console.log(compChoice);
+  return compChoice;
+}
+
 function generatePassword() {
   // Have user select password categories and length.
   var userChoice = selectCategory();
   console.log(userChoice);
-  
+
   var userLength = selectLength();
   console.log(userLength);
 
@@ -114,58 +186,15 @@ function generatePassword() {
     }
   }
 
-  var password =[];
+  // Generate and validate random password.
+  var password = randomPassword(allowable, userLength);
+  var compChoice = validation(password, userChoice);
 
-  // Display generated password in alert or written on page
-  for (let index = 0; index < userLength; index++) {
-    password = password + allowable[Math.floor(Math.random()*allowable.length)];
-  }
-  console.log(password); 
-  
-  // Ensure password matches user selection.
-  var validation = [];
-  var compChoice = [false, false, false, false];
-  for (let index = 0; index < password.length; index++) {
-      validation[index] = password.charCodeAt(index); // https://www.w3schools.com/js/js_string_methods.asp
-    }
-  console.log(validation);
-  
-  // Validate Lower Case
-  if (userChoice[0]) {
-    for (let index = 0; index < validation.length; index++) {
-      if ((97 <= validation[index]) && (validation[index] <= 122)) {
-        compChoice[0] = true;
-        break;
-      }
-    }
-  }
-  console.log(compChoice);
-  // Validate Upper Case
-  if (userChoice[1]) {
-    for (let index = 0; index < validation.length; index++) {
-      if ((65 <= validation[index]) && (validation[index] <= 90)) {
-        compChoice[1] = true;
-        break;
-      }
-    }
-  }
-  console.log(compChoice);
-  // Validate Number
-  if (userChoice[2]) {
-    for (let index = 0; index < validation.length; index++) {
-      if ((48 <= validation[index]) && (validation[index] <= 57)) {
-        compChoice[2] = true;
-        break;
-      }
-    }
+  // Generate new password until a valid one is chosen. Validate each time
+  while ((compChoice[0] !== userChoice[0]) || (compChoice[1] !== userChoice[1]) || (compChoice[2] !== userChoice[2]) || (compChoice[3] !== userChoice[3])) {
+    password = randomPassword(allowable, userLength);
+    compChoice = validation(password, userChoice);
   }
 
-  console.log(compChoice);
-
-  // Generate new password until a valid one is chosen
-  // do {
-  //   password = generatePassword();
-  // } while ((compChoice[0] !== userChoice[0]) || (compChoice[1] !== userChoice[1]) || (compChoice[2] !== userChoice[2]) || (compChoice[3] !== userChoice[3]));
-  
   return password;
 }
